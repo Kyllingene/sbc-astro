f43b2e097129d52508022285691732e906cb4cd7
Kyllingene <fuzzymuffin343@gmail.com>
2023-03-05 21:57:18 -0800

    Trailing slash weirdness, and with gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyllingene.github.io/sbc-astro',
-  base: '/sbc-astro'
+  base: '/sbc-astro',
+  trailingSlash: 'always',
 });
